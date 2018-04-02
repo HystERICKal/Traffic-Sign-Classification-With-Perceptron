5398e51e531e5a61d7864971a493b8b3bebad316
hibestil <halilibrahimbestil@hotmail.com>
2018-04-03 00:38:36 +0300

    update

--- classifier.py
+++ classifier.py
@@ -1,17 +1,24 @@
 import numpy as np
 def main():
-    print ("hello")
+    ## Create train data
     traindata = np.array([[0,0,1],[0,1,1],[1,0,1],[1,1,1]])
     trainlabel = np.array([0,0,0,1])
-    w =  np.zeros(3)
+    ## Train 
+    w = train(traindata,trainlabel,100)
+    ## Create test data
+    testdata = np.array([1,1,1])
+    ## Print test result
+    print ( test(w,testdata)    )
+
+def train(traindata,trainlabel,epochs=100):
+    w =  np.zeros(len(traindata[0]))
     eta = 1
-    for epoch in range(0,100):
+    for epoch in range(0,epochs):
         for x,y in zip(traindata,trainlabel):
             r = x.dot(w) >= 0
             hata = y-r
             w+= eta * hata * x
-
-    ## TEST
-    test = np.array([1,0,1])
-    print ( test.dot(w) >= 0 )
-main()
+    return w
+def test(w,testdata):
+    return testdata.dot(w) >= 0 
+main() 
